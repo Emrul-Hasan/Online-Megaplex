5c956d1ae87a90c5984618d78fe0c5600f8d6aea
Emrul-Hasan <imrulhasan003@gmail.com>
2022-05-03 20:06:39 +0600

    Shipment add

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
-// import Inventory from './components/Inventory/Inventory';
+ import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
-// import RequireAuth from './components/RequireAuth/RequireAuth';
-// import Shipment from './components/Shipment/Shipment';
+import RequireAuth from './components/RequireAuth/RequireAuth';
+ import Shipment from './components/Shipment/Shipment';
 import Shop from './components/Shop/Shop';
 import SignUp from './components/SignUp/SignUp';
 
@@ -19,7 +19,7 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        {/* <Route path='/inventory' element={
+        <Route path='/inventory' element={
           <RequireAuth>
             <Inventory></Inventory>
           </RequireAuth>
@@ -28,7 +28,7 @@ function App() {
           <RequireAuth>
             <Shipment></Shipment>
           </RequireAuth>
-        }></Route> */}
+        }></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import logo from '../../images/Logo.svg';
-// import './Header.css';
-
-// const Header = () => {
-//     return (
-//         <nav className='header'>
-//             <img src={logo} alt="" />
-//             <div>
-//                 <Link to="/shop">Shop</Link>
-//                 <Link to="/orders">Orders</Link>
-//                 <Link to="/inventory">Inventory</Link>
-//                 <Link to="/about">About</Link>
-//             </div>
-//         </nav>
-//     );
-// };
-
-// export default Header;
 
 import { signOut } from 'firebase/auth';
 import React from 'react';
